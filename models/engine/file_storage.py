aa8ba7edb77533fa119ccd1c91953afca5ceef8a
eric <boateng.eric44@gmail.com>
2023-09-19 17:19:56 +0000

    debugging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,14 +58,4 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-
-    def close(self):
-        """Call the reload method."""
-        self.reload()
-
+    def delete
