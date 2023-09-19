baaeef112cfb315246fa6921c6c15309501a4469
eric <boateng.eric44@gmail.com>
2023-09-19 12:44:33 +0000

    updated dbstorage_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+import models
+import os
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
     name = ""
