6f6550f195dca22600a0fb266e82fda2a43d6a6d
eric <boateng.eric44@gmail.com>
2023-10-05 11:32:40 +0000

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,36 +14,36 @@ def do_deploy(archive_path):
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
-        If the _file doesn't exist at archive_path or an error occurs - False.
+        If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
-    _file = archive_path.split("/")[-1]
-    _name = _file.split(".")[0]
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(_file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(_file, _name)).failed is True:
+           format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(_file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(_name, _name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(_name)).failed is True:
+           format(name)).failed is True:
         return False
     return True
